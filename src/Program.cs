5d5235bc1dbd11b896ca8331f8c17d507d9160e0
kc-kiran369 <kckiran0690@gmail.com>
2022-08-07 19:45:41 +0545

    First Setup

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VisualProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatingSystem os = Environment.OSVersion;
+            Console.WriteLine("Current OS Information:\n");
+            Console.WriteLine("Platform: {0:G}", os.Platform);
+            Console.WriteLine("Version String: {0}", os.VersionString);
+            Console.WriteLine("Version: {0}", os.Version);
+        }
+    }
+}
